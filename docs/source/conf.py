ac05ece3b4f62e4029aecfaaefffcaa6ab69d4d1
yangyihe <yangchichi@126.com>
2024-06-20 02:22:44 +0800

    rtd update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,14 +2,17 @@
 #
 # LazyLLM documentation build configuration file
 
+import pkg_resources
 import os
 import sys
-# import sphinx_rtd_theme
-# html_theme = 'sphinx_rtd_theme'
-# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_theme = 'sphinx_material'
 
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath("../"))
+
+version = "v" + pkg_resources.get_distribution("lazyllm").version
+
+project = u'LazyLLM'
+copyright = u'2024, SenseTime Research'
+author = u'SenseTime Research'
 
 extensions = [
     "sphinx.ext.autodoc",
@@ -27,44 +30,68 @@ extensions = [
     # "sphinxcontrib.gtagjs",
 ]
 
-source_suffix = '.rst'
-master_doc = 'index'
+# automodapi requires this to avoid duplicates apparently
+numpydoc_show_class_members = False
 
-# General information about the project.
-project = u'LazyLLM'
-copyright = u'2024, SenseTime Research'
-author = u'SenseTime Research'
-version = u'0.1.0'
-release = u'0.1.0 rc0'
 
-language = 'cn'
-exclude_patterns = []
-pygments_style = 'sphinx'
-todo_include_todos = True
-html_static_path = ['_static']
-html_sidebars = {
-    '**': [
-        'relations.html',  # needs 'show_related': True theme option to display
-        'searchbox.html',
-    ]
-}
-htmlhelp_basename = 'LazyLLMdoc'
-latex_elements = {
-}
+myst_heading_anchors = 5
+# TODO: Fix the non-consecutive header level in our docs, until then
+# disable the sphinx/myst warnings
+suppress_warnings = ["myst.header"]
 
-latex_documents = [
-    (master_doc, 'LazyLLM.tex', u'LazyLLM Documentation',
-     u'SenseTime Research', 'manual'),
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "DOCS_README.md"]
+html_theme = "furo"
+html_title = project + " " + version
+html_static_path = ["_static"]
+
+html_css_files = [
+    "css/custom.css",
+    "css/algolia.css",
+    "https://cdn.jsdelivr.net/npm/@docsearch/css@3",
 ]
-
-
-man_pages = [
-    (master_doc, 'lazyllm', u'LazyLLM Documentation',
-     [author], 1)
+html_js_files = [
+    "js/mendablesearch.js",
+    (
+        "https://cdn.jsdelivr.net/npm/@docsearch/js@3.3.3/dist/umd/index.js",
+        {"defer": "defer"},
+    ),
+    ("js/algolia.js", {"defer": "defer"}),
+    ("js/leadfeeder.js", {"defer": "defer"}),
 ]
+html_permalinks_icon = "#"
+nb_execution_mode = "off"
+autodoc_pydantic_model_show_json_error_strategy = "coerce"
+nitpicky = True
 
-texinfo_documents = [
-    (master_doc, 'LazyLLM', u'LazyLLM Documentation',
-     author, 'LazyLLM', 'One line description of project.',
-     'Miscellaneous'),
-]
+# language = 'cn'
+# exclude_patterns = []
+# pygments_style = 'sphinx'
+# todo_include_todos = True
+# html_static_path = ['_static']
+# html_sidebars = {
+#     '**': [
+#         'relations.html',  # needs 'show_related': True theme option to display
+#         'searchbox.html',
+#     ]
+# }
+# htmlhelp_basename = 'LazyLLMdoc'
+# latex_elements = {
+# }
+
+# latex_documents = [
+#     (master_doc, 'LazyLLM.tex', u'LazyLLM Documentation',
+#      u'SenseTime Research', 'manual'),
+# ]
+
+
+# man_pages = [
+#     (master_doc, 'lazyllm', u'LazyLLM Documentation',
+#      [author], 1)
+# ]
+
+# texinfo_documents = [
+#     (master_doc, 'LazyLLM', u'LazyLLM Documentation',
+#      author, 'LazyLLM', 'One line description of project.',
+#      'Miscellaneous'),
+# ]
